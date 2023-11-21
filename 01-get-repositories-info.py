db38861e99c340d32c1e88d34cdd84215a31063b
cmdsilva25 <83644309+cmdsilva25@users.noreply.github.com>
2023-11-21 00:27:15 -0300

    Add files via upload

--- 01-get-repositories-info.py
+++ 01-get-repositories-info.py
@@ -1,59 +1,97 @@
+###################################################################
+# imports
+###################################################################
 import requests
 import time
 import json
 
-def get_repository_info(query):
-    url = f'https://api.github.com/search/repositories?q={query}&page=1'
-    response = requests.get(url)
 
-    if response.status_code == 200:
-        data = response.json()
-        total_count = data['total_count']
-        total_pages = int(response.headers['Link'].split(',')[-1].split(';')[0].split('page=')[-1].strip('>"'))
-        return total_count, total_pages
-    else:
-        print(f"Erro na solicitação: {response.status_code}")
-        return 0, 0
-
-def get_repositories(query, page):
-    url = f'https://api.github.com/search/repositories?q={query}&page={page}'
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        return response.json()['items']
-    else:
-        print(f"Erro na solicitação: {response.status_code}")
-        return None
-
+###################################################################
+# main function
+###################################################################
 def main():
-    query = "starknet cairo"  # String de pesquisa
+    # open index of repositories: each line stores one repository
+    with open('index-of-repositories.txt', 'r', encoding='utf-8') as input_file:
+        repos_list = input_file.readlines()
 
-    total_count, total_pages = get_repository_info(query)
-    if total_count == 0 or total_pages == 0:
-        print("Não foi possível obter a informação sobre repositórios.")
-        return
-
-    print(f"Total de repositórios disponíveis: {total_count}")
-    print(f"Total de páginas disponíveis: {total_pages}")
-
+    # create output json file
     json_file = open('01-repositories-info.json', 'w', encoding='utf-8')
 
+    # process each line
     counter = 0
-    for page in range(0, total_pages+1):
-        repositories = get_repositories(query, page)
+    for repo in repos_list:
+        get_repository_summary(repo, json_file)
+        counter += 1
+        print(f'Repo: {counter}/{len(repos_list)}')
 
-        if repositories:
-            print(f"\nRepositórios na página {page} com as strings 'starknet' e 'cairo':\n")
-            for repo in repositories:
-                line = json.dumps(repo)
-                json_file.write( line + '\n' )
-                time.sleep(1)
-                counter+=1
-                print('counter=',counter, ' page=', page)
-        else:
-            print("Nenhum repositório encontrado.")
+    # close output json file
     json_file.close()
 
+    # print the output file created
+    print('Created file: "01-get-repositories-info.json"')
 
+
+###################################################################
+# function receives the list of repositories or users
+# and issues http requests to get the repository data summary
+###################################################################
+def get_repository_summary(param, json_file):
+    # remove whitespaces and convert to lowercase
+    param = str(param).strip().lower()
+
+    # find out if this is an "user" or "repo"
+    url = param.split('/')
+
+    if len(url)==4:
+        # this is a user: we must get the list of repos
+        user = url[3]
+        api_url = f'https://api.github.com/users/{user}/repos'
+        response = make_http_request(api_url)
+        if response == None:
+            return
+        for repo in response:
+            line = json.dumps(repo)
+            json_file.write( line + '\n' )
+    elif len(url)==5:
+        # this is a repo
+        user = url[3]
+        repo = url[4]
+        api_url = f'https://api.github.com/repos/{user}/{repo}'
+        response = make_http_request(api_url)
+        if response == None:
+            return
+        for repo in response:
+            line = json.dumps(repo)
+            json_file.write( line + '\n' )
+    else:
+        # garbage
+        print (f'Unexpected input: {param}')
+    return
+
+
+###################################################################
+# helper function to make http requests using the REST API
+###################################################################
+def make_http_request( api_url ):
+    # always sleep before making a request
+    time.sleep(10)
+    # make http request
+    response = requests.get(api_url)
+    print(api_url)
+    #print(response.status_code)
+    #print(response.headers)
+
+    # check for success
+    if response.status_code == 200:
+        return response.json()
+
+    # error while making http request
+    print(f"Error while making http request status {response.status_code}")
+    return None #or better exit(1)?
+
+
+###################################################################
+# helper to call the main function
+###################################################################
 if __name__ == "__main__":
     main()
